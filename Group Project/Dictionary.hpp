7e27c4342f183867cde52d7601ca83fb93af379f
lenovo <yn739843@dal.ca>
2017-11-19 17:15:04 -0400

    2

--- Group Project/Dictionary.hpp
+++ Group Project/Dictionary.hpp
@@ -6,7 +6,6 @@ using namespace std;
 
 class Dictionary{
 protected:
-    string dict[26][26][5000];//holds all words with forst two letters indexed
     char alpha[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M',
         'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};//holds alphabet
 public:
@@ -14,6 +13,5 @@ public:
     ~Dictionary();
     void fill();
     bool isWord(string word);
-    void test();
 };
 #endif
